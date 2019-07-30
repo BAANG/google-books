0924b741ae1185b07a96bda21a4c219a6b7754ff
BAANG <chcelestino@gmail.com>
2019-07-29 20:02:58 -0700

    Create routes; require in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require ('mongoose')
+const routes = require('./routes')
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +14,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes)
 
 // Send every other request to the React app
 // Define any API routes before this runs
