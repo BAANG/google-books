8f5bbb1289ee0e851846baafa80be5915372cbe9
BAANG <chcelestino@gmail.com>
2019-07-29 19:35:30 -0700

    Connect to mongo via mongoose; Create book schema

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require ('mongoose')
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +19,8 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
